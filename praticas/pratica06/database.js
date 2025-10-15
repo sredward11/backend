f9d7daad68f66a43b1a66696091aa853d35695fa
Sr.Edward <caduzecavalim@gmail.com>
2025-10-15 10:00:14 -0300

    Commit pratica06

---
+++ praticas/pratica06/database.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { MongoClient } = require("mongodb");
+
+const url = `mongodb+srv://edwardcarl:Shablau123@cluster0.5jpdpjr.mongodb.net/`
+
+let db = null;
+
+const client = new MongoClient(url);
+
+async function conectar() {
+    try {
+    if (db == null) {
+         await client.connect()
+         db = client.db("agenda")
+    }
+    console.log("Conectado ao MOongoDB");
+    return db;
+  } catch (e) {
+    console.log("Erro ao conectar no MongoDB", e. message)
+  }
+}
+module.exports = conectar
